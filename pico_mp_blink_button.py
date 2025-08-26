0ac190a93f0cb23c27a4b2a33279516a3520ebf3
Jeremy Underwood <underwood.j248@gmail.com>
2025-08-26 07:34:07 -0400

    alternate two LEDs

--- pico_mp_blink_button.py
+++ pico_mp_blink_button.py
@@ -2,7 +2,9 @@ import machine
 import utime
 
 # Create a Pin object for the onboard LED (set as an OUTPUT pin)
-led = machine.Pin("LED", machine.Pin.OUT)
+led1 = machine.Pin("LED", machine.Pin.OUT)
+
+led2= machine.Pin(15, machine.Pin.OUT)
 # Create a Pin object for a push button on GPIO 14 configured as an INPUT pin
 # Set default state to High
 btn = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
@@ -10,7 +12,7 @@ btn = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
 # Define fast and slow blink speeds
 SLOW = 1.0  # 1 second delay
 FAST = 0.1  # 0.1 second delay
-DEBOUNCE_MS = 25. # 25 ms debounce time to avoid false button presses
+DEBOUNCE_MS = 25  # 25 ms debounce time to avoid false button presses
 
 # Function to check if the button is pressed
 def pressed():
@@ -19,13 +21,21 @@ def pressed():
     utime.sleep_ms(DEBOUNCE_MS)  # Delay to filter out mechanical bouncing
     return btn.value() == 0  # Return True if button is Low (pressed)
 
+state = 0
 # Main program loop
 while True:
     # If button is pressed, blink LED fast; otherwise blink slow
     delay = FAST if pressed() else SLOW
-    # Turn LED on
-    led.value(1)
-    utime.sleep(delay) # Wait for delay time
-    # Turn LED off
-    led.value(0)
-    utime.sleep(delay) # Wait for delay time
+    # alternate LEDs
+    state ^= 1
+    led1.value(state)
+    led2.value(1 - state)
+    utime.sleep(delay)
+
+
+    # # Turn LED on
+    # led1.value(1)
+    # utime.sleep(delay) # Wait for delay time
+    # # Turn LED off
+    # led1.value(0)
+    # utime.sleep(delay) # Wait for delay time
